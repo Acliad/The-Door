2268e91f69f8e296c6be871b81a70b43b0394103
Isaac Rex <isaaclrex@gmail.com>
2023-12-26 11:13:56 -1000

    Added lanczos based shifting

--- src/doordisplay/frames/utils.py
+++ src/doordisplay/frames/utils.py
@@ -1,4 +1,66 @@
 from PIL import Image
+import numpy as np
+from scipy.signal import convolve
+
+def _lanczos_kernel(x, a=3):
+    """
+    Lanczos kernel function.
+    :param x: Input values.
+    :param a: Size of the kernel, typically 2 or 3.
+    :return: Lanczos kernel values over x.
+    """
+    L = np.zeros_like(x)
+    L[x == 0] = 1.0
+    kernel_idx = abs(x) < a
+    L[kernel_idx] = np.sinc(x[kernel_idx]) * np.sinc(x[kernel_idx] / a)
+    return L
+
+def shift(pos1:np.ndarray, shiftx:float, shifty:float, shift_eps=0.01, **kwargs):
+    """
+    Shifts the canvas by the specified amount in the x and y directions.
+
+    TODO: 
+        - Add modes for wrap, clip, and extend
+        - Add choices for interpolation strategy
+
+    Args:
+        pos1 (np.ndarray): The input canvas to be shifted.
+        shiftx (float): The amount of shift in the x direction.
+        shifty (float, optional): The amount of shift in the y direction.
+        shift_eps (float, optional): The threshold for considering fractional shift. Values less than this will be 
+                                     considered int shifts. Defaults to 0.01.
+        **kwargs: Additional keyword arguments.
+            a (int): The size of the Lanczos kernel. Defaults to 3.
+
+    Returns:
+        np.ndarray: The shifted canvas.
+    """
+    a = kwargs.get('a', 3)
+
+    # Get the fractional and integer parts of the shift
+    shiftx_frac = shiftx % 1 if shiftx >= 0 else -(-shiftx % 1)
+    shifty_frac = shifty % 1 if shifty >= 0 else -(-shifty % 1)
+    shiftx_int = int(shiftx)
+    shifty_int = int(shifty)
+
+    if shiftx_frac >= shift_eps or shifty_frac >= shift_eps:
+        # Create the Lanczos kernel for convolution
+        lki = np.arange(-a + 1, a + 1, 1)
+        lanczos_kernelx = _lanczos_kernel(shiftx_frac - lki)
+        lanczos_kernely = _lanczos_kernel(shifty_frac - lki)
+        lanczos_kernelxy = lanczos_kernelx[:, None] @ lanczos_kernely[:, None].T
+
+        # Convolve the image with the Lanczos kernel
+        pos2 = convolve(pos1, lanczos_kernelxy[:, :, None], mode='same')
+        # Clip negative values to 0
+        pos2 = np.maximum(pos2, 0)
+    else:
+        pos2 = pos1
+
+    # Shift the image by the integer part of the shift
+    pos2 = np.roll(pos2, (shifty_int, shiftx_int), axis=(0, 1))
+
+    return pos2.astype(pos1.dtype)
 
 def scale_fit(image:Image.Image, width:int, height:int) -> Image.Image:
     """Scales an image to fit within a frame while maintaining the aspect ratio
@@ -32,7 +94,6 @@ def scale_fit(image:Image.Image, width:int, height:int) -> Image.Image:
     frame = Image.new('RGB', (width, height))
     frame.paste(image_resized, ((width - new_width) // 2, (height - new_height) // 2))
     return frame
-
 
 def clip(image: Image.Image, width: int, height: int, position: str = 'center') -> Image.Image:
     """
